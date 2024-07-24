598d0d1297ad848081f2b9ef8e873141531afc9e
Your Name <iliashklm@gmail.com>
2024-07-24 16:23:27 +0300

    added orders

---
+++ js/order.js
@@ -0,0 +1,56 @@
+const orderTabs = document.querySelectorAll(".order-options a");
+const priceTabs = document.querySelectorAll(".price-options a");
+const rightArrow = document.querySelector(".order-options .right-arrow svg");
+const leftArrow = document.querySelector(".order-options .left-arrow svg");
+const orderTabsList = document.querySelector(".order-options ul");
+const priceTabsList = document.querySelector(".price-options ul");
+const rightArrowContainer = document.querySelector(".order-options .right-arrow");
+const leftArrowContainer = document.querySelector(".order-options .left-arrow");
+
+const removeAllActiveClasses = (tabs) => {
+    tabs.forEach((tab) => {
+        tab.classList.remove("active");
+    });
+};
+
+orderTabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        removeAllActiveClasses(orderTabs);
+        tab.classList.add("active");
+    });
+});
+
+priceTabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        removeAllActiveClasses(priceTabs);
+        tab.classList.add("active");
+    });
+});
+
+const manageIcons = () => {
+    if (orderTabsList.scrollLeft >= 20) {
+        leftArrowContainer.classList.add("active");
+    } else {
+        leftArrowContainer.classList.remove("active");
+    }
+
+    let maxScrollValue = orderTabsList.scrollWidth - orderTabsList.clientWidth -20;
+
+    if(orderTabsList.scrollLeft >= maxScrollValue) {
+        rightArrowContainer.classList.remove("active");
+    } else { 
+        rightArrowContainer.classList.add("active");
+    }
+};
+
+rightArrow.addEventListener("click", () => {
+    orderTabsList.scrollLeft += 200;
+    manageIcons();
+});
+
+leftArrow.addEventListener("click", () => {
+    orderTabsList.scrollLeft -= 200;
+    manageIcons();
+});
+
+orderTabsList.addEventListener("scroll", manageIcons)
